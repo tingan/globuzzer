2a5d4b7bb384b3000d92ac929ec8ae0d0450ca24
tingan tang <bend90@gmail.com>
2018-12-30 16:51:32 +0200

    add calendar jquery ui

--- public/js/custom-min.js
+++ public/js/custom-min.js
@@ -1,1 +1,1 @@
-$(function(){$(".dropdown-menu a").on("click",function(){$(this).parent().parent().find(".dropdown-text").html($(this).html())})});
+$(function(){$(".dropdown-menu a").on("click",function(){$(this).parent().parent().find(".dropdown-text").html($(this).html())}),$(".datepicker").datepicker()});
--- public/js/custom.js
+++ public/js/custom.js
@@ -4,4 +4,9 @@ $(function() {
   $('.dropdown-menu a').on('click', function () {
     $(this).parent().parent().find('.dropdown-text').html($(this).html());
   });
+
+  $('.datepicker').datepicker();
+
+
+
 });
