7a924ffbcfbd00f76579678fa6596ef76397454a
tingan tang <bend90@gmail.com>
2019-01-01 20:28:43 +0200

    use constant

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var data = require('./data/data.json')
+const data = require('./data/data.json')
 const express = require('express');
 const open = require('open');
 const app = express();
 app.use(express.static('public'));
 
 app.get('/countries', (req, res) =>
-    res.json(data)
+  res.json(data)
 );
 app.listen(3000, function () {
-console.log('App started on port 3000');
-open('http://localhost:3000');
+  console.log('App started on port 3000');
+  open('http://localhost:3000');
 });
